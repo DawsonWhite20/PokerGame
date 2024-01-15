63a2680abfde4307a0f32f424d884a6e23dcc2ee
dwhit <dwhite005@icloud.com>
2024-01-15 16:59:28 -0500

    Added objects and starting procedures in Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
 
-public class Game implements DeckTypes{
+public class Game implements Hand {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Research and add try/catch blocks
-        System.out.println("Welcome to your Poker game!\n\nHow many players do you have?");
+        // Asks user for number of players
+        System.out.print("Welcome to your Poker game!\n\nHow many players do you have: ");
         int numPlayers = scanner.nextInt();
+        System.out.println();
+        String[] players = new String[numPlayers];
+        scanner.nextLine();
 
+        // Player enters user names
         System.out.println("Please give a name to each player.");
         for(int i = 0;i < numPlayers;i++) {
-            System.out.println("Enter player " + i+1+ "'s name: ");
+            int currentPlayer = i + 1;
+            System.out.print("Enter player " + currentPlayer + "'s name: ");
+            String playerName = scanner.nextLine();
+            players[i] = playerName;
         }
+        System.out.println();
+
+
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+
+    private int money;
+    private Card cards;
+
+    public Player() {
+        money = 500;
+    }
+
+    public Player(int money) {
+        this.money = money;
+    }
+}
